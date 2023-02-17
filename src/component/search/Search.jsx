fbd2e13042ac5b41dcdc4db0ffb83f4fe567e349
Ruwan Ranaweera <rwnranaweera@gmail.com>
2023-02-17 07:40:03 +0530

    country search part adding

---
+++ src/component/search/Search.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+const Search = ({onSearch}) => {
+    const [input, setInput] = useState('');
+    const submitHandler = (e)=>{
+        e.preventDefault()
+        onSearch(input);
+    }
+  return <form onSubmit = {submitHandler}>
+    <input type="text" placeholder='Search for a Country' value={input} onChange={(e)=>
+    setInput(e.target.value)} />
+  </form>
+
+  
+}
+
+export default Search;
